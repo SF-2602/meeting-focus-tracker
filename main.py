cc3c2b0e8e32e8fb1526d6d869bf6fbceabc5d10
Sean Darren Timothy <seandarren00@gmail.com>
2026-03-19 16:36:48 +0800

    Fix Local & Deployment

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ app.add_middleware(
     allow_origins=[
         "https://meeting-focus-tracker.vercel.app",
         "https://forespent-elba-unsmoulderingly.ngrok-free.dev",
-        "http://localhost:3000",                      
+        "http://localhost:5173",                      
         "http://127.0.0.1:3000",
     ],
     allow_credentials=True,
